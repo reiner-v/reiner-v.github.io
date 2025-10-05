107fc33595ff99f55495233fa63c49c567f92975
reiner-v <reinerrvaldez@gmail.com>
2025-10-05 18:40:39 +0800

    Commit #5
    Added assets, edited index

--- main.js
+++ main.js
@@ -1,53 +1,49 @@
 // Your existing projects data
 const projects = {
-     "project-0":{
+    
+     "project-7":{
         title: "iNeed Parking Application",
-        description:"This Project is parking management implementing a  user session control, in-chat message and real time location.",
+        description:"This Project's objective is to monitor and track the air quality within the cabuyao using a device and web application. It contains various sensors like DHT, MQ Series and gps module.",
         tools:[ "Android Studio", "Visual Studio Code","MongoDB"],
-        images:["assets/Image/Project-Image/project-0/1.png","assets/Image/Project-Image/project-0/2.png","assets/Image/Project-Image/project-0/3.png","assets/Image/Project-Image/project-0/5-1.png","assets/Image/Project-Image/project-0/5-2.png","assets/Image/Project-Image/project-0/5-3.png"]
+        images:["assets/Image/Project-Image/project-7/1.png","assets/Image/Project-Image/project-7/2.png","assets/Image/Project-Image/project-7/3.png","assets/Image/Project-Image/project-7/5-1.png","assets/Image/Project-Image/project-7/5-2.png","assets/Image/Project-Image/project-7/5-3.png"]
     },
-    "project-1":{
-        title: "IoT Based Air Quality Monitoring System",
+    "project-6":{
+        title: "IOT BASED AIR QUALIT MONITORING SYSTEM",
         description:"This Project's objective is to monitor and track the air quality within the cabuyao using a device and web application. It contains various sensors like DHT, MQ Series and gps module.",
         tools:[ "Arduino IDE", "Visual Studio Code", ],
-        images:["assets/Image/Project-Image/project-1/0.png","assets/Image/Project-Image/project-1/1.png","assets/Image/Project-Image/project-1/2.png","assets/Image/Project-Image/project-1/3.png","assets/Image/Project-Image/project-1/4.png","assets/Image/Project-Image/project-1/5.png","assets/Image/Project-Image/project-1/6.png"]
+        images:["assets/Image/Project-Image/project-6/0.png","assets/Image/Project-Image/project-6/1.png","assets/Image/Project-Image/project-6/2.png","assets/Image/Project-Image/project-6/3.png","assets/Image/Project-Image/project-6/4.png","assets/Image/Project-Image/project-6/5.png","assets/Image/Project-Image/project-6/6.png"]
     },
-    "project-2":{
+    "project-5":{
         title: "SMART ACCESS CONTROL AND MONITORING SYSTEM",
         description:"This project incorporates an RFID sensor and key to authenticate authorized persons within an establishment. When an authorized UID is scanned by the RFID, a green light will turn on. If the sensor detects an unauthorized attempt, a red light will turn on, and the alarm system will engage.",
         tools:["Visual Studio Community","xampp","PhpMyAdmin","ArduinoIDE"],
-        images:["assets/Image/Project-Image/project-2/0.png","assets/Image/Project-Image/project-2/1.png","assets/Image/Project-Image/project-2/2.png","assets/Image/Project-Image/project-2/3.png","assets/Image/Project-Image/project-2/4.png","assets/Image/Project-Image/project-2/5.png"]
+        images:["assets/Image/Project-Image/project-5/0.png","assets/Image/Project-Image/project-5/1.png","assets/Image/Project-Image/project-5/2.png","assets/Image/Project-Image/project-5/3.png","assets/Image/Project-Image/project-5/4.png","assets/Image/Project-Image/project-5/5.png"]
     },
-    "project-3":{
+    "project-4":{
         title: "LIGHT MOTION SENSOR WITH MANUAL OVERRIDE",
         description:"This project aims to assist in illuminating areas automatically or manually based on the user's preference and records data each time the LDR detects a change in light levels.",
         tools:["Visual Studio Community","xampp","PhpMyAdmin","ArduinoIDE"],
-        images:["assets/Image/Project-Image/project-3/1.png","assets/Image/Project-Image/project-3/2.png"]
+        images:["assets/Image/Project-Image/project-4/1.png","assets/Image/Project-Image/project-4/2.png"]
     },
-    "project-4":{
+    "project-3":{
         title: "LEHITIMO: Get that Salary",
         description:"This Project will allow the admin to create and update the employees information including name, roles, salary and such. They will be also to search for a specific employee also delete an employee.",
         tools:["Visual Studio Community","xampp","PhpMyAdmin"],
-        images:["assets/Image/Project-Image/project-4/1.png","assets/Image/Project-Image/project-4/2.png","assets/Image/Project-Image/project-4/3.png","assets/Image/Project-Image/project-4/4.png","assets/Image/Project-Image/project-4/5.png","assets/Image/Project-Image/project-4/6.png","assets/Image/Project-Image/project-4/7.png","assets/Image/Project-Image/project-4/8.png","assets/Image/Project-Image/project-4/9.png"]
+        images:["assets/Image/Project-Image/project-3/1.png","assets/Image/Project-Image/project-3/2.png","assets/Image/Project-Image/project-3/3.png","assets/Image/Project-Image/project-3/4.png","assets/Image/Project-Image/project-3/5.png","assets/Image/Project-Image/project-3/6.png","assets/Image/Project-Image/project-3/7.png","assets/Image/Project-Image/project-3/8.png","assets/Image/Project-Image/project-3/9.png"]
     },
-    "project-5":{
+    "project-2":{
         title: "RENTALIGHT: ONLINE RESERVATION",
         description:"This Online Application and Reservation System simplifies finding and booking properties. Clients can easily register, find properties, make reservations, manage cancellations, and track their booking status.",
         tools:["Visual Studio Community","SQL Database"],
-        images:["assets/Image/Project-Image/project-5/1.png","assets/Image/Project-Image/project-5/2.png","assets/Image/Project-Image/project-5/3.png"]
+        images:["assets/Image/Project-Image/project-2/1.png","assets/Image/Project-Image/project-2/2.png","assets/Image/Project-Image/project-2/3.png"]
     },
-    "project-6":{
+    "project-1":{
         title: "ABC COMPANY: MANAGE EMPLOYEE",
         description:"This system offers streamlined employee management, covering everything from adding and updating employee details, managing payroll and positions, to organizing departments.",
         tools:["PyCharm"],
-        images:["assets/Image/Project-Image/project-6/1.png","assets/Image/Project-Image/project-6/2.png","assets/Image/Project-Image/project-6/3.png","assets/Image/Project-Image/project-6/4.png","assets/Image/Project-Image/project-6/5.png","assets/Image/Project-Image/project-6/6.png"]
+        images:["assets/Image/Project-Image/project-1/1.png","assets/Image/Project-Image/project-1/2.png","assets/Image/Project-Image/project-1/3.png","assets/Image/Project-Image/project-1/4.png","assets/Image/Project-Image/project-1/5.png","assets/Image/Project-Image/project-1/6.png"]
     },
-     "project-7":{
-        title: "Jumble Word Game: A Happy Place",
-        description:"This Game Project is developed on the platform of windows form. It has a leaderboard and creation of player by inputting a name to continue to play. Each level will increase the difficulty as the word gets longer.",
-        tools:["Visual Studio Community"],
-        images:["assets/Image/Project-Image/project-7/0.png","assets/Image/Project-Image/project-7/1.png","assets/Image/Project-Image/project-7/2.png"]
-    },
+
 };
 
 
@@ -72,7 +68,7 @@ function populateProjectCards() {
     //determine the limit of the card display
     if (currentPage.includes("index.html")){
         limit = 6;
-        projectPad.style.padding = "5rem";
+        projectPad.style.padding = "5rem 2rem";
         if(mediaQuery.matches){
             projectPad.style.padding = "0";
         }
@@ -87,7 +83,6 @@ function populateProjectCards() {
                 // Create project card HTML
                 const projectCard = document.createElement('article');
                 projectCard.className = 'project-card';
-                console.log(`DEBUG: ${project.image}`);
                 projectCard.innerHTML = `
                     <div class="project-image">
                         <img src="${project.images[0]}" alt="${project.title}" onerror="this.style.display='none'; this.nextElementSibling.style.display='block';">
@@ -97,7 +92,7 @@ function populateProjectCards() {
                         <h3>${project.title}</h3>
                         <p>${project.description.substring(0, 100)}${project.description.length > 100 ? '...' : ''}</p>
                         <hr>
-                        <p><b>Software Tools:</b></p>
+                        <p><b>SOFTWARE TOOLS:</b></p>
                         <ul>${project.tools.map(tool => `<li>${tool}</li>`).join('')}</ul>
                     </div>
                     <button id="${projectKey}" class="project-button">View Project</button>
@@ -110,7 +105,7 @@ function populateProjectCards() {
     attachProjectButtonListeners();
 }
 
-// Function to attach event listeners to project buttons
+// Function to attach event listeners to view project buttons
 function attachProjectButtonListeners() {
     const projectButtons = document.querySelectorAll(".project-button");
     
@@ -201,21 +196,6 @@ function attachProjectButtonListeners() {
     });
 }
 
-// Modal close functionality
-const span = document.getElementsByClassName("close")[0];
-const projectModal = document.getElementById("project-modal");
-
-if (span) {
-    span.onclick = function() {
-        projectModal.style.display = "none";
-    }
-}
-
-window.onclick = function(event) {
-    if (event.target == projectModal) {
-        projectModal.style.display = "none";
-    }
-}
 
 // Function to initialize swiper functionality
 function initializeSwiper(totalImages) {
@@ -300,7 +280,105 @@ function initializeSwiper(totalImages) {
     }
 }
 
-// Initialize the project cards when the DOM is loaded
+//About Me Layer Project
+function profileImageControl(){
+    const profile_card = document.getElementsByClassName("card-float")[0];
+    const goal_card = document.getElementsByClassName("card-float")[1];
+    const interest_card = document.getElementsByClassName("card-float")[2];
+
+    const inital_image =  document.querySelectorAll(".display-block");
+    const hidden_image = document.querySelectorAll(".display-none");
+
+    const goal_img_1 = document.getElementsByClassName("img-layer")[6];
+    const goal_img_2 = document.getElementsByClassName("img-layer")[7];
+
+    const interest_img = document.getElementsByClassName("img-layer")[8];
+
+    profile_card.addEventListener("mouseenter",function(){
+        inital_image.forEach(element => {
+            element.classList.remove("display-block");
+            element.classList.add("display-none");
+        });
+        hidden_image.forEach(element => 
+        {
+            element.classList.remove("display-none");
+            element.classList.add("display-block");
+        });
+
+    });
+
+    profile_card.addEventListener("mouseleave",function(){
+        inital_image.forEach(element => {
+            element.classList.remove("display-none");
+            element.classList.add("display-block");
+        });
+        hidden_image.forEach(element => 
+        {
+            element.classList.remove("display-block");
+            element.classList.add("display-none");
+        });
+    });
+
+    goal_card.addEventListener("mouseenter",function(){
+        goal_img_1.classList.add("transform-scale");
+        goal_img_2.classList.add("transform-scale");
+    });
+    goal_card.addEventListener("mouseleave",function(){
+        goal_img_1.classList.remove("transform-scale");
+        goal_img_2.classList.remove("transform-scale");
+    });
+
+    interest_card.addEventListener("mouseenter",function(){
+        interest_img.classList.add("transform-scale");
+    });
+    interest_card.addEventListener("mouseleave",function(){
+        interest_img.classList.remove("transform-scale");
+    });
+}
+
+//Skill & Certi
+function enlargeImgModal(){
+    const certificateList = document.getElementsByClassName('certificates-list')[0];
+    const certificateItems = certificateList.querySelectorAll('li');
+    const enlargeImg = document.getElementById("enlarge-img" );
+
+    certificateItems.forEach(item => {
+        item.addEventListener('click',()=>{
+            enlargeModal.style.display='block';
+            enlargeImg.src= item.querySelector('img').src;
+        });
+    });
+}
+
+ 
+// Modal close functionality
+const span = document.querySelectorAll(".close");
+const projectModal = document.getElementById("project-modal");
+const enlargeModal = document.getElementById('enlarge-modal');
+
+
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('close')) {
+        projectModal.style.display = "none";
+        enlargeModal.style.display = "none";
+    }
+});
+
+window.onclick = function(event) {
+    if (event.target == projectModal) {
+        projectModal.style.display = "none";
+    }
+    else if(event.target == enlargeModal){
+         enlargeModal.style.display = "none";
+    }
+}
+
+
+
+// Initialize when the DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
     populateProjectCards();
+    profileImageControl();
+    enlargeImgModal();
 });
+
